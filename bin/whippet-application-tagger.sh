3b634080d0a5dce7c22db613f1ed7a05e6a19aa2
Rob Skilling <rob@dxw.com>
2021-08-16 16:46:53 +0100

    Add script to add whippet-app topic to all whippet apps
    This script will loop through all dxw repos on GitHub, looking for those
    with a `whippet.lock` file in the root. If found, it will add the
    "whippet-app" topic to the repo.

---
+++ bin/whippet-application-tagger.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# exit on failures
+set -e
+set -o pipefail
+
+# get all repos with whippet.lock in root
+REPOS=$(gh api -X GET search/code -f q='filename:whippet.lock org:dxw path:/' --paginate  -q '[ .items[].repository.full_name ]' | jq -r '.[]')
+for REPO in $REPOS; do
+  # skip archived repos
+  REPO_IS_ARCHIVED=$(eval "gh api -X GET repos/$REPO -q '.archived'")
+  if [ $REPO_IS_ARCHIVED = true ]; then
+    continue
+  fi
+
+  # get the repo topics
+  TOPICS=$(eval "gh api -X GET repos/$REPO/topics -H accept:application/vnd.github.mercy-preview+json -q '[ .names[] ]' | jq -r '.[]'")
+  # skip if it already includes the whippet-app topic
+  if [[ ! " ${TOPICS[*]} " =~ "whippet-app" ]]; then
+    touch input.json
+    NEW_TOPICS=${TOPICS}
+    NEW_TOPICS+=('whippet-app')
+    printf '%s\n' "${NEW_TOPICS[@]}" | jq -R . | jq -s {"names":.} > input.json
+    eval "gh api -X PUT repos/$REPO/topics -H accept:application/vnd.github.mercy-preview+json --input input.json --silent"
+    echo "$REPO updated"
+    unset NEW_TOPICS
+  fi
+
+done
+
+# cleanup
+rm input.json
