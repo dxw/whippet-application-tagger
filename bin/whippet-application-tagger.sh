398563024dc8f7ee531380b1cff865a5b7445168
Rob Skilling <rob@dxw.com>
2022-04-26 09:43:43 +0100

    Output what is happening during script run
    We are seeing increasing, but intermittent, failures when running this
    action on GitHub (though the script still seems to be completing
    successfully when run locally).
    
    The only error we're getting is `gh: Not Found (HTTP 404)`, which
    suggests the script is making an API call to an endpoint that doesn't
    exist. To work out exactly when this is happening, this commit adds more
    output from the script, so we can tell what it's doing when it fails.

--- bin/whippet-application-tagger.sh
+++ bin/whippet-application-tagger.sh
@@ -5,11 +5,14 @@ set -e
 set -o pipefail
 
 # get all repos with whippet.lock in root
+echo "Getting list of repos to update..."
 REPOS=$(gh api -X GET search/code -f q='filename:whippet.lock org:dxw path:/' --paginate  -q '[ .items[].repository.full_name ]' | jq -r '.[]')
 for REPO in $REPOS; do
+  echo "Checking $REPO"
   # skip archived repos
   REPO_IS_ARCHIVED=$(eval "gh api -X GET repos/$REPO -q '.archived'")
   if [ "$REPO_IS_ARCHIVED" = true ]; then
+    echo "$REPO is archived, skipping"
     continue
   fi
 
@@ -17,6 +20,7 @@ for REPO in $REPOS; do
   TOPICS=$(eval "gh api -X GET repos/$REPO/topics -H accept:application/vnd.github.mercy-preview+json -q '[ .names[] ]' | jq -r '.[]'")
   # skip if it already includes the whippet-app topic
   if [[ ! " ${TOPICS[*]} " =~ "whippet-app" ]]; then
+    echo "$REPO does not have tag"
     touch input.json
     NEW_TOPICS=${TOPICS}
     if [ ${#TOPICS} -gt 0 ]; then
@@ -24,11 +28,14 @@ for REPO in $REPOS; do
     else
       NEW_TOPICS=('whippet-app')
     fi
+    echo "Adding tag to $REPO"
     printf '%s\n' "${NEW_TOPICS[@]}" | jq -R . | jq -s '{ "names": . }' > input.json
     eval "gh api -X PUT repos/$REPO/topics -H accept:application/vnd.github.mercy-preview+json --input input.json --silent"
     echo "$REPO updated"
     unset NEW_TOPICS
     rm input.json
+  else
+    echo "$REPO already has tag"
   fi
 
 done
